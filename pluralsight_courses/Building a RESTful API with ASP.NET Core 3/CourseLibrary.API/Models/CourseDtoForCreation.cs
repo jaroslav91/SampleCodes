85d147cb737818a6f47777107ba5852b6a81930d
jaroslav <jaroslav91@gmail.com>
2020-07-06 20:01:18 +0200

    Add validation attributes to dto object

--- pluralsight_courses/Building a RESTful API with ASP.NET Core 3/CourseLibrary.API/Models/CourseDtoForCreation.cs
+++ pluralsight_courses/Building a RESTful API with ASP.NET Core 3/CourseLibrary.API/Models/CourseDtoForCreation.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.ValidationAttributes;
+
 namespace CourseLibrary.API.Models
 {
+    [CourseTitleMustBeDifferentFromDescriptionAttribute(ErrorMessage = "The provided description should be different from the title.")]
     public class CourseDtoForCreation
-    {        
+    {
+        [Required(ErrorMessage = "You should fill out title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more then 100 characters.")]
         public string Title { get; set; }
 
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
         public string Description { get; set; }
     }
 }
---
+++ pluralsight_courses/Building a RESTful API with ASP.NET Core 3/CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.Models;
+
+namespace CourseLibrary.API.ValidationAttributes
+{
+    public class CourseTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            var course = (CourseDtoForCreation)validationContext.ObjectInstance;
+
+            if (course.Title == course.Description)
+            {
+                return new ValidationResult(
+                    ErrorMessage, new[] { nameof(CourseDtoForCreation) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
