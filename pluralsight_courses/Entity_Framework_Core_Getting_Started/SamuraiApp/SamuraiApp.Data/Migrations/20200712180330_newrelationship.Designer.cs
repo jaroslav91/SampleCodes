2c244166da53de96014fbe47ff3e93d29517411e
jaroslav <jaroslav91@gmail.com>
2020-07-12 22:29:40 +0200

    Add Domain Model

--- pluralsight_courses/Entity_Framework_Core_Getting_Started/SamuraiApp/SamuraiApp.Data/Migrations/20200712180330_newrelationship.Designer.cs
+++ pluralsight_courses/Entity_Framework_Core_Getting_Started/SamuraiApp/SamuraiApp.Data/Migrations/20200712180330_newrelationship.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SamuraiApp.Data;
 
 namespace SamuraiApp.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20200712180330_newrelationship")]
+    partial class newrelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- pluralsight_courses/Entity_Framework_Core_Getting_Started/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ pluralsight_courses/Entity_Framework_Core_Getting_Started/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -1,19 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
-  public class SamuraiContext : DbContext
-  {
-    public DbSet<Samurai> Samurais { get; set; }
-    public DbSet<Quote> Quotes { get; set; }
-    public DbSet<Clan> Clans { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public class SamuraiContext : DbContext
     {
-      optionsBuilder.UseSqlite(
-          "Data Source = samurai-sqlLite.db");
-          
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        public static readonly ILoggerFactory ConsoleLoggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+            .AddFilter((category, level) =>
+                category == DbLoggerCategory.Database.Command.Name
+                && level == LogLevel.Information)
+            .AddConsole();
+        });
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+            .UseLoggerFactory(ConsoleLoggerFactory)
+            .EnableSensitiveDataLogging(true)
+            .UseSqlite(
+                "Data Source = samurai-sqlLite.db", optionsBuilder => optionsBuilder.MaxBatchSize(150));
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(x => new { x.SamuraiId, x.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+        }
     }
-  }
+
 }
