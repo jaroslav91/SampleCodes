6bfef6d48e8e83ab53d420d1654aaecee7604660
jaroslav <jaroslav91@gmail.com>
2020-08-23 14:39:18 +0200

    add net core razor project

---
+++ dotnet/consoleApps/SimpleCharCounter/CharCounter.Tests/CharCounterTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace CharCounter.Tests
+{
+    public class CharCounterTests
+    {
+
+            /*
+            Algorytm zliczania ilość znaków, ignorując ich wielkość.
+            Wyświetla je w kolejności:
+            - ilość od największej do najmniejszej,
+            - alfabetycznie (0-9, A-Z).
+            Zadanie wykonaj z użyciem List i bez użycia Listy tylko tablic (dwa algorytmy).
+            
+            */
+
+        [Fact]
+        public void Test1()
+        {
+            var inputString = "asdfasdasa";
+
+            ArrayFunction
+
+        }
+    }
+}
---
+++ dotnet/consoleApps/SimpleCharCounter/CharCounter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace charCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Algorytm zliczania ilość znaków, ignorując ich wielkość.
+            Wyświetla je w kolejności:
+            - ilość od największej do najmniejszej,
+            - alfabetycznie (0-9, A-Z).
+            Zadanie wykonaj z użyciem List i bez użycia Listy tylko tablic (dwa algorytmy).
+            
+            */
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
